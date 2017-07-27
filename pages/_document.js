6b020439a0df5e2af0daf39200f43860a806836f
Max Fierro <elmasse@gmail.com>
2017-07-27 16:23:43 -0300

    lang="en" attr in html

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
 
   render () {
     return (
-      <html>
+      <html lang="en">
         <Head>
           <title>Nextein</title>
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
