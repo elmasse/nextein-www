6a63c2d6a01e7a6373a323e2ee81ef6d304173a3
Max Fierro <elmasse@gmail.com>
2017-09-11 23:37:46 -0300

    icons for navigation

--- components/navigation.js
+++ components/navigation.js
@@ -2,12 +2,19 @@
 import React from 'react'
 import styled from 'emotion/react'
 
+import Github from './icons/github'
+import Npm from './icons/npm'
+
 export default (props) => {
   return (
     <Nav {...props}>
-      <a href="/guides/01-getting-started">guides</a>
-      <a href="https://github.com/elmasse/nextein">github</a>
-      <a href="https://www.npmjs.com/package/nextein">npm</a>
+      <a href="/guides/01-getting-started">Guides</a>
+      <a href="https://github.com/elmasse/nextein">
+        <Github fill="#e4e4e4" width="25"/>
+      </a>
+      <a href="https://www.npmjs.com/package/nextein">
+        <Npm fill="#e4e4e4" width="35"  style={{marginTop: '-5px'}} />
+      </a>
     </Nav>
   )
 }
@@ -18,7 +25,7 @@ const Nav = styled('nav')`
   padding: 0 30px;
   
   & > a {
-    padding: 10px;
+    padding: 15px;
     color: #999;
     text-decoration: none;
   }
