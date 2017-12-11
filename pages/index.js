e9ec3ffe625977bfdb5fae87e8b1da7436bf4dc7
Max Fierro <elmasse@gmail.com>
2017-12-11 19:08:59 -0300

    refactored styles

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ const Index = withIndexSections(({ posts }) => {
       font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Lucida Grande", sans-serif;
       font-weight: 100
     }
-    a { 
+    a {
       color: #666; 
       font-weight: 200;
       text-decoration-color: #aaa;
@@ -43,10 +43,10 @@ const Index = withIndexSections(({ posts }) => {
       {
         sections.map((post, idx) => {
           const { className, title } = post.data
-          const kind = (idx % 2) ? 'odd' : 'even'
+          const kind = (idx % 2) === 0 ? 'reversed' : ''
           return (
-            <Section className={kind} key={`section-${idx}`}>
-              <Title className={className} >{title}</Title>
+            <Section className={classnames(kind, className)} key={`section-${idx}`}>
+              <Title>{title}</Title>
               <Content {...post} />
             </Section>
           )
@@ -71,29 +71,22 @@ const Section = styled('section')`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-evenly;
   min-height: 70vh;
   padding: 120px 10vw;
-
-  > * {
-    flex: 1;
-  }
-  
 
   &:last-of-type {
     padding-bottom: 120px;
   }
 
-  &.odd {    
-    background-image: radial-gradient(circle at center, #fff 0%,  #e9e9e9 100%);
-    text-shadow: 0 0 1px #eee;
-  }
+  background-image: radial-gradient(circle at center, #fff 0%,  #e9e9e9 100%);
+  text-shadow: 0 0 1px #eee;
 
-  &.even {
+  &.reversed {
     flex-direction: row-reverse;    
     background-image: radial-gradient(circle at center , rgb(68, 60, 60) 0%, #272121 100%);
     text-shadow: 0 0 1px #000;
-    
+
     > p {
       text-align: right;
       color: #e4e4e4;
@@ -101,7 +94,21 @@ const Section = styled('section')`
   }
 `
 
+const Title = styled('h1')`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column-reverse;
+
+  text-align: center;
+  font-weight: 100;
+  font-size: 3.5em;
+  color: #f63;
+`
+
 const Content = styled(PostContent)`
+  flex: 1;
+
   font-size: 1.5em;
   font-weight: 200;
   line-height: 1.5em;
@@ -109,47 +116,11 @@ const Content = styled(PostContent)`
   a, a:hover, a:visited {
     color: #666;
     text-decoration: none;
-  }
-
-  a { font-weight: 400; }
-
-`
-
-const Title = styled('h1')`
-  text-align: center;
-  position: relative;
-  display: flex;
-  flex-direction: column-reverse;
-  font-weight: 100;
-  font-size: 3em;
-  color: #f63;
-
-  &.nextein {
-    font-size: 4.5em;
-  }
-
-  &.md::after {    
-    content: '#_';
-    font-size: 100px;
-    color: #aaa;
     font-weight: 400;
-    font-size: 4em;
   }
 
-  &.next::after {
-    content: '</>';
-    text-shadow: 0 0 1px #000;
-    font-size: 100px;
-    color: #e5e5e5;
-    font-weight: 400; 
-    font-size: 4em;   
+  code {
+    background: #e4e4e4;
+    padding: 10px;    
   }
-
-  // &.try-it::after {
-  //   content: '!';
-  //   font-size: 100px;
-  //   color: #aaa;
-  //   font-weight: 400; 
-  //   font-size: 4em;   
-  // }
 `
