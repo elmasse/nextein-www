4033e6476502e8a52dd49a0d6c7aeaf5024bc2ba
Max Fierro <elmasse@gmail.com>
2018-02-16 20:17:46 -0300

    footer cosmetic changes

--- components/footer.js
+++ components/footer.js
@@ -7,12 +7,14 @@ import Npm from './icons/npm'
 export default () => {
   return (
     <Footer>
-      <Brand>Nextein</Brand>
-      <Notice>&copy; 2017 - Max Fierro</Notice>
-      <Social>
-        <a href="https://github.com/elmasse/nextein"><Github fill="#564949" width="25" style={{padding: '10px 10px 0 0'}}/></a>
-        <a href="https://www.npmjs.com/package/nextein"><Npm fill="#564949" width="45" /></a>
-      </Social>
+      <Wrapper>
+        <Brand>Nextein</Brand>
+        <Notice>&copy; 2017 - Max Fierro</Notice>
+        <Social>
+          <a href="https://github.com/elmasse/nextein"><Github fill="#564949" width="35" /></a>
+          <a href="https://www.npmjs.com/package/nextein"><Npm fill="#564949" width="45" /></a>
+        </Social>
+      </Wrapper>
       <BuiltWithLove />
     </Footer>
   )
@@ -28,33 +30,48 @@ const Footer = styled('footer')`
   position: relative;
   display: flex;
   flex-direction: column;
+  align-items: stretch;
   background: #272121;
-  height: 250px;
-  padding-top: 40px;
-  > * {
-    padding-left: 20px;    
-  }
+  min-height: 250px;
+`
+
+const Wrapper = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  flex: 1;
+  padding: 32px;
+`
+const Separator = styled('div')`
+  height: 0px;
+  border: 1px solid #f63;
+  border-bottom: none;
+  margin: 1.3em 0;
 `
 
 const Brand = styled('div')`
   color: #f63;
-  font-size: 1.8em
+  font-size: 2.5em
 `
 
 const Notice = styled('div')`
-  font-size: .8em;
+  font-size: .65em;
   color: #f1f1f1;
-  padding-bottom: 20px;
+  text-transform: uppercase;
+  letter-spacing: .06em;
 `
 
 const Social = styled('div')`
   flex: 3;
   font-size: .8em;
-  padding-top: 10px;
-  align-items: flex-start;
+  align-items: center;
   display: flex;
   flex-direction: row;
   
+  a {
+    padding: 5px;
+  }
+
   a:hover svg {
     fill: #9a8888;
   }
@@ -63,10 +80,9 @@ const Social = styled('div')`
 const Built = styled('div')`
   background-color: #131010;
 
-  padding: 10px 0;
+  padding: 16px 0;
   align-self: stretch;
   text-align: center;
-  
 
   font-size: .75em;
 
