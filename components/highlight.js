bcd200add68239d046b9a80e5b72ca8396942e6d
Max Fierro <elmasse@gmail.com>
2018-05-28 10:19:57 -0300

    Improve bundles by reducing highlight.js languages. Related to #10

--- components/highlight.js
+++ components/highlight.js
@@ -1,50 +1,64 @@
-import hljs from 'highlight.js';
-import React from 'react';
+
+import React from 'react'
+import hljs from 'highlight.js/lib/highlight'
+import bash from 'highlight.js/lib/languages/bash'
+import javascript from 'highlight.js/lib/languages/javascript'
+import json from 'highlight.js/lib/languages/json'
+import markdown from 'highlight.js/lib/languages/markdown'
+import yaml from 'highlight.js/lib/languages/yaml'
+import xml from 'highlight.js/lib/languages/xml'
+
+hljs.registerLanguage('bash', bash)
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('json', json)
+hljs.registerLanguage('markdown', markdown)
+hljs.registerLanguage('yaml', yaml)
+hljs.registerLanguage('xml', xml)
 
 class Highlight extends React.Component {
-    componentDidMount() {
-        this.highlightCode();
+  componentDidMount() {
+    this.highlightCode()
+  }
+
+  componentDidUpdate() {
+    this.highlightCode()
+  }
+
+  highlightCode() {
+    const nodes = this.el.querySelectorAll('pre code')
+
+    for (let i = 0; i < nodes.length; i++) {
+      hljs.highlightBlock(nodes[i])
+    }
+  }
+
+  setEl = (el) => {
+    this.el = el
+  }
+
+  render() {
+    const { children, className, element: Element, innerHTML } = this.props
+    const props = { ref: this.setEl, className }
+
+    if (innerHTML) {
+      props.dangerouslySetInnerHTML = { __html: children }
+      if (Element) {
+        return <Element {...props} />
+      }
+      return <div {...props} />
     }
 
-    componentDidUpdate() {
-        this.highlightCode();
+    if (Element) {
+      return <Element {...props}>{children}</Element>
     }
-
-    highlightCode() {
-        const nodes = this.el.querySelectorAll('pre code');
-
-        for (let i = 0; i < nodes.length; i++) {
-            hljs.highlightBlock(nodes[i])
-        }
-    }
-
-    setEl = (el) => {
-        this.el = el;
-    };
-
-    render() {
-        const {children, className, element: Element, innerHTML} = this.props;
-        const props = { ref: this.setEl, className };
-
-        if (innerHTML) {
-            props.dangerouslySetInnerHTML = { __html: children };
-            if (Element) {
-                return <Element {...props} />;
-            }
-            return <div {...props} />;
-        }
-
-        if (Element) {
-            return <Element {...props}>{children}</Element>;
-        }
-        return <pre ref={this.setEl}><code className={className}>{children}</code></pre>;
-    }
+    return <pre ref={this.setEl}><code className={className}>{children}</code></pre>
+  }
 }
 
 Highlight.defaultProps = {
-    innerHTML: false,
-    className: null,
-    element: null,
-};
+  innerHTML: false,
+  className: null,
+  element: null,
+}
 
-export default Highlight;
+export default Highlight
