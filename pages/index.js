5e37289514b9d6414faa3bd810cf6f830b724667
Max Fierro <elmasse@gmail.com>
2018-07-03 23:35:29 -0300

    terminals and wording for front post

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import Head from 'next/head'
 import { withPostsFilterBy, inCategory } from 'nextein/posts'
 import { Content as PostContent } from 'nextein/post'
 import styled from 'react-emotion'
@@ -8,6 +9,8 @@ import Header from '../components/header'
 import Navigation from '../components/navigation'
 import Footer from '../components/footer'
 import withPageView from '../components/analytics'
+import Code from '../components/code'
+import Terminal from '../components/code/terminal'
 
 const classnames = (...args) => args.join(' ')
 const sortByOrder = (a, b) => a.data.order - b.data.order
@@ -20,6 +23,10 @@ const Index = withIndexSections(({ posts }) => {
 
   return (
     <Main>
+      <Head>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atom-one-light.min.css" />
+      </Head>
+
       <Navigation style={{ position: 'absolute', width: '100vw' }}/>      
       <Header />
       {
@@ -29,7 +36,12 @@ const Index = withIndexSections(({ posts }) => {
           return (
             <Section className={classnames(kind, className)} key={`section-${idx}`}>
               <Title>{title}</Title>
-              <Content {...post} />
+              <Content
+                {...post}
+                renderers={{
+                  code: WindowWrapper
+                }}
+              />
             </Section>
           )
         })
@@ -40,6 +52,11 @@ const Index = withIndexSections(({ posts }) => {
 })
 
 export default withPageView(Index)
+
+
+const WindowWrapper = props => (
+  <Terminal><Code {...props}/></Terminal>
+)
 
 // --- styled ---
 
@@ -64,12 +81,24 @@ const Section = styled('section')`
   background-image: radial-gradient(circle at center, #fff 0%,  #e9e9e9 100%);
   text-shadow: 0 0 1px #eee;
 
-  &.reversed {
-    flex-direction: row-reverse;    
-    background-image: radial-gradient(circle at center , rgb(68, 60, 60) 0%, #272121 100%);
-    text-shadow: 0 0 1px #000;
+  & h2 {
+    font-size: 1.1em;
+    font-weight: 100;
+  }
 
-    > p {
+  &.reversed {
+    border-top: 1px solid #e4e4e4;
+    border-bottom: 1px solid #e4e4e4;
+    flex-direction: row-reverse;
+
+    background-image: radial-gradient(circle at center , rgb(78, 60, 60) 0%, #272121 100%);
+    
+    & h1 {
+      text-shadow: 0 0 1px #000;
+    }
+
+    & p {
+      text-shadow: 0 0 1px #000;
       text-align: right;
       color: #e4e4e4;
     }
@@ -103,6 +132,14 @@ const Content = styled(PostContent)`
 
   code {
     background: #e4e4e4;
-    padding: 10px;    
+    padding: 10px;
+  }
+`
+
+const TerminalContainer = styled('div')`
+  border-radius: 5px;
+  box-shadow: 2px 8px 16px rgba(0,0,0, 0.3);
+  & .hljs {
+    display: block;
   }
 `
