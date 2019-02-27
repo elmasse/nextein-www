03520b60d2530816c2f28185a0b462c9e6b0f148
Max Fierro <elmasse@gmail.com>
2019-02-27 00:19:57 -0300

    Fix extract-toc plugins.

--- plugins/extract-toc.js
+++ plugins/extract-toc.js
@@ -3,40 +3,48 @@ const visit = require('unist-util-visit')
 
 const { inCategory } = require('nextein/posts')
 
-const extractHeaders = (post) => {
-  const { content } = post
-
-  const appendValue = item => textNode => {
-    item.value += `${textNode.value.trim()} `
-  }
-  const appendHref = item => node => {
-    if (node.tagName === 'a') {
-      item.href = node.properties.href
-    }
-  }
-  
-  return selectAll('h1,h2,h3,h4,h5,h6', content).map(node => {
-    const item = {
-      type: node.tagName,
-      value: ''
-    }
-    visit(node, 'text', appendValue(item))
-    visit(node, 'element', appendHref(item))
-    return { ...item, value: item.value.trim() }
-  })
+const headingsByLevel = level => {
+  return Array.from(Array(level), (h, idx) => `h${idx+1}`)
 }
 
-const filter =(categories = [], post) => {
+const appendValue = item => textNode => {
+  item.value += `${textNode.value.trim()} `
+}
+const appendHref = item => node => {
+  if (node.tagName === 'a') {
+    item.href = node.properties.href
+  }
+}
+
+const extractHeaders = (post, { headings }) => {
+  const { content } = post
+  const toc = []
+  visit(content, 'element', node => {
+    if (headings.includes(node.tagName)) {
+      const item = {
+        type: node.tagName,
+        value: ''
+      }
+      visit(node, 'text', appendValue(item))
+      visit(node, 'element', appendHref(item))
+      toc.push({ ...item, value: item.value.trim() })
+    }
+  })
+  return toc;
+}
+
+const filter = (categories = [], post) => {
   for ( const category of categories ) {
-    if (inCategory(category)(post)) return true
+    if (inCategory(category, { includeSubCategories: true })(post)) return true
   }
   return false
 }
 
-module.exports.transform = async ({ categories = true }, posts) => {
+module.exports.transform = async ({ categories = true, maxDepth = 6 }, posts) => {
+  const headings = headingsByLevel(maxDepth)
   return posts.map(post => {
     if (categories && filter(categories, post)) {
-      post.data.toc = extractHeaders(post)
+      post.data.toc = extractHeaders(post, { headings })
     }
     return post
   })
