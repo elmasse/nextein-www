2c03d9b65e2f5e0f69a9b5ed50108906192088d1
Max Fierro <elmasse@gmail.com>
2019-07-08 20:32:18 -0300

    Add favicon.

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
+          <link href="/static/images/favicon.ico" rel="icon" type="image/x-icon" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Open+Sans:300,400,600|Montserrat:300,400,600" rel="stylesheet" />
         </Head>
