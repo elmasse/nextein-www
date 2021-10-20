e57cf659b5ce836299ffab94748df47cac4f72ee
Max Fierro <elmasse@gmail.com>
2021-10-20 14:56:31 -0300

    Improve accessibility on button variant highlight

--- src/components/button.js
+++ src/components/button.js
@@ -6,11 +6,11 @@ export const Button = forwardRef(({ children, variant='normal', border=true, cla
       ref={ref}
       type='text'
       className={[
-        'flex items-center justify-center h-10 px-6',
+        'flex items-center justify-center h-12 px-6',
         'uppercase font-normal',
         'rounded-md',
         variant === 'normal' && 'bg-gray-100 hover:bg-gray-300 text-gray-900',
-        variant === 'highlight' && 'bg-action text-gray-900',
+        variant === 'highlight' && 'bg-action text-gray-200',
         border && 'border-gray-900 border',
         'hover:shadow-sm',
         className].filter(Boolean).join(' ')}
